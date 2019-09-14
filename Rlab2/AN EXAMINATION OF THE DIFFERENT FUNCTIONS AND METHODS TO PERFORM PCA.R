236e003cc0518d499dde0a4fbde00fca91c8d2a3
Your Name <you@example.com>
2019-09-14 11:36:37 +0000

    wer

---
+++ Rlab2/AN EXAMINATION OF THE DIFFERENT FUNCTIONS AND METHODS TO PERFORM PCA.R
@@ -0,0 +1,3 @@
+qqnorm(mtcars[,1]) #A Q-Q plot is a scatterplot created by plotting two sets of quantiles against one another. If both sets of quantiles came from the same distribution, we should see the points forming a line that’s roughly straight.\
+qqline(mtcars[,1])
+
